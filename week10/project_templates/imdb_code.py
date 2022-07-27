8e2fd775865a58ea498c938d6f1dee87bb1f661b
insdout <49560124+insdout@users.noreply.github.com>
2022-07-27 19:34:23 +0300

    labs initial commit

--- week10/project_templates/imdb_code.py
+++ week10/project_templates/imdb_code.py
@@ -5,8 +5,13 @@ from imdb_helper_functions import helper_function_example
 
 
 def get_actors_by_movie_soup(cast_page_soup, num_of_actors_limit=None):
-    # your code here
-    return # your code here
+    actors_list = []
+    cast_list = cast_page_soup.find('table', class_='cast_list')
+    actors = cast_list.find_all('td', class_='')
+    for actor in actors[0:num_of_actors_limit]:
+        actors_list.append(tuple((actor.find('a').text.strip(), actor.find('a')['href'])))
+        # print(actor.find('a').text.strip(), urllib.parse.urljoin(url,actor.find('a')['href']))
+    return actors_list
 
 
 def get_movies_by_actor_soup(actor_page_soup, num_of_movies_limit=None):
